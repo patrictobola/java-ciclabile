f0a21a1d3b98dd7a6a8fbe58b07e4cc80aff154f
patrictobola <tobola92@gmail.com>
2023-11-17 18:02:22 +0100

    done bonus and fixed code

--- src/org/java/Ciclatore.java
+++ src/org/java/Ciclatore.java
@@ -1,29 +1,42 @@
 package org.java;
 
-import java.util.NoSuchElementException;
-
 public class Ciclatore {
-	
+
 	private int[] list;
 	private int currentIndex;
-	
+	private int size;
+
+	public Ciclatore() {
+		this.list = new int[0];
+		this.size = 0;
+		this.currentIndex = 0;
+	}
+
 	public Ciclatore(int[] list) {
-		
+
 		this.list = list;
 		this.currentIndex = 0;
 	}
-	
+
 	public int nextNumber() {
 		int number = list[currentIndex];
-		currentIndex = (currentIndex + 1) % list.length;
+//		currentIndex = (currentIndex + 1) % list.length;
+		currentIndex++;
 		return number;
 	}
-	
+
 	public Boolean hasNextNumber() {
-		return currentIndex < list.length - 1;
+		return currentIndex < list.length;
 	}
-	
-	
+
+	public void addElemento(int newElemento) {
+		int[] newArray = new int[size + 1];
+		System.arraycopy(list, 0, newArray, 0, size);
+		newArray[size] = newElemento;
+		list = newArray;
+		size++;
+	}
+
 	public int[] getList() {
 		return list;
 	}
--- src/org/java/Main.java
+++ src/org/java/Main.java
@@ -6,9 +6,16 @@ public class Main {
 		
 		int[] numbers = {1, 2, 5, 8, 9, 15, 123};
 		Ciclatore ciclatore = new Ciclatore(numbers);
+		Ciclatore ciclatoreProva = new Ciclatore();
+		ciclatoreProva.addElemento(4);
+		ciclatoreProva.addElemento(15);
 		
 		while(ciclatore.hasNextNumber()) {
 			int number = ciclatore.nextNumber();
+			System.out.println("Numero: " + number);
+		}
+		while(ciclatoreProva.hasNextNumber()) {
+			int number = ciclatoreProva.nextNumber();
 			System.out.println("Numero: " + number);
 		}
 	}
